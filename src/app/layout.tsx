e640da21c60f4adf86007d3a57dc63f232ad104f
Omar Espinoza <omaresp35@gmail.com>
2025-04-20 14:19:42 -0700

    work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,30 @@ import { inter } from './fonts';
 export const metadata: Metadata = {
   title: 'Omar Espinoza | Portfolio',
   description: 'Omar Espinoza\'s portfolio website showcasing projects and skills',
+  icons: {
+    icon: '/images/icon.png', // This will be used for browser tabs
+    shortcut: '/images/icon.png', // For legacy browsers
+    apple: '/images/icon.png', // For Apple devices
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://omarsportfolio.com', // Replace with your actual URL
+    title: 'Omar Espinoza | Portfolio',
+    description: 'Omar Espinoza\'s portfolio website showcasing projects and skills',
+    siteName: 'Omar Espinoza Portfolio',
+    images: [{
+      url: '/images/heropage.png', // This will be used for social media sharing
+      width: 1200,
+      height: 630,
+      alt: 'Omar Espinoza Portfolio',
+    }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Omar Espinoza | Portfolio',
+    description: 'Omar Espinoza\'s portfolio website showcasing projects and skills',
+    images: ['/images/heropage.png'], // This will be used for Twitter sharing
+  },
 };
 
 export default function RootLayout({
